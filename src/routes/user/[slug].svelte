d82d6ba4a71f312b0672838e3b0db0bfbc7a98c7
Stiv <pablo1012@outlook.com>
2020-09-02 19:54:13 -0300

    test with slugs and routes

---
+++ src/routes/user/[slug].svelte
@@ -0,0 +1,24 @@
+<script context="module">
+  export async function preload(page, session) {
+    const { slug } = page.params;
+    const res = await this.fetch(
+      `https://jsonplaceholder.typicode.com/todos/${slug}`
+    );
+    const user = await res.json();
+    return { user };
+  }
+</script>
+
+<script>
+    export let user;
+</script>
+
+<svelte:head>
+    <title>{user.title}</title>
+</svelte:head>
+
+<h1>
+    {user.title}
+</h1>
+
+
