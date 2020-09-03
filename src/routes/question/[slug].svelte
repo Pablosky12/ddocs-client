8a5edc3cc7248bc2edfdb5870319df2aac503d1a
Stiv <pablo1012@outlook.com>
2020-09-03 20:34:02 -0300

    add basic question page

---
+++ src/routes/question/[slug].svelte
@@ -0,0 +1,24 @@
+<script context="module">
+  export async function preload(page, session) {
+    const { slug } = page.params;
+    const res = await this.fetch(
+      `http://lit-cliffs-44994.herokuapp.com/question/${slug}`
+    );
+    const question = await res.json();
+    console.log(question);
+    return { question };
+  }
+</script>
+
+<script>
+  export let question;
+</script>
+
+<svelte:head>
+  <title>{question.question_text}</title>
+</svelte:head>
+
+<h1>{question.text}</h1>
+{#each question.answers as answer}
+  <li>{answer.text}</li>
+{/each}
