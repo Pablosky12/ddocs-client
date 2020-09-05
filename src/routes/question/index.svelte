f50cccd342a87c97faa534319b61987af366bba6
Stiv <pablo1012@outlook.com>
2020-09-04 22:02:05 -0300

    add questions list

---
+++ src/routes/question/index.svelte
@@ -0,0 +1,24 @@
+<script context="module">
+  export async function preload(page, session) {
+    const { slug } = page.params;
+    const res = await this.fetch(
+      `http://lit-cliffs-44994.herokuapp.com/question`
+    );
+    const latestQuestions = await res.json();
+    console.log(latestQuestions);
+    return { latestQuestions };
+  }
+</script>
+
+<script>
+  export let latestQuestions;
+</script>
+
+<svelte:head>
+  <title>Questions</title>
+</svelte:head>
+
+<h1>{latestQuestions.total}</h1>
+{#each latestQuestions.questions as question}
+  <li>{question.text}</li>
+{/each}
