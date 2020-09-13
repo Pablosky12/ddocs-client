e19915a84750cd9533b54de8f2ee08b347a1c1d6
Stiv <pablo1012@outlook.com>
2020-09-13 20:32:25 -0300

    Add list item styling for questions

---
+++ src/components/QuestionItem.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let question;
+</script>
+
+<style>
+    li {
+        padding: 1em;
+        border: 1px solid #00000016;
+        border-radius: 5px;
+        list-style: none;
+        box-shadow: 5px 5px 5px -5px rgba(0,0,0,0.2);
+        margin: 10px 0;
+    }
+
+</style>
+
+<li>
+    <h3>{question.text.charAt(0).toUpperCase()}{question.text.slice(1)}</h3>
+</li>
